bff1b64480d86b201e1b0598cd37971f8638a088
달리는 다람쥐 <48404941+KamaTAEWOO@users.noreply.github.com>
2023-02-27 18:27:25 +0900

    2023-02-27 1. Test Commit

--- app/src/main/java/com/project/androidretrofitexample/MainActivity.kt
+++ app/src/main/java/com/project/androidretrofitexample/MainActivity.kt
@@ -2,7 +2,7 @@ package com.project.androidretrofitexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
